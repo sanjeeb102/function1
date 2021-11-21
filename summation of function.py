2d42fbf684a4489e938cdd496157d8ca392297bf
sanjeeb102 <75179577+sanjeeb102@users.noreply.github.com>
2021-11-21 11:27:26 +0530

    summation

---
+++ summation of function.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[4]:
+
+
+def sum(numbers):
+    total = 0
+    for x in numbers:
+        total +=x
+    return total
+
+print(sum((8,2,3,0,7)))
+        
+
